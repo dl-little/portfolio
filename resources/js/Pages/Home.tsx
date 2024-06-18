ecb4dcc7c9f49e96edb73014485c710dda4f6fbb
dl-little <little.douglas22@gmail.com>
2024-06-18 00:03:23 -0400

    Better home marquee

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -35,14 +35,29 @@ const Rotating = styled.span`
 	overflow: hidden;
 	transition-property: width;
 	transition-duration: 2s;
+	scrollbar-width: none; /* Firefox */
+	-ms-overflow-style: none;  /* Internet Explorer 10+ */
+	height: 1em;
+	scroll-behavior: smooth;
+	scroll-snap-type: y mandatory;
+	scroll-snap-points-y: repeat(1em);
+	&.paused {
+		overflow-y: scroll;
+	}
 `;
 
 const RotatingTitle = styled.span`
-	margin-top: -.1em;
-	display: none;
+	opacity: 0;
+	transition-property: opacity;
+	transition-duration: 1s;
+
+	.paused & {
+		scroll-snap-align: start;
+		opacity: 1;
+	}
 
 	&.active {
-		display: block;
+		opacity: 1;
 		max-width: fit-content;
 	}
 `;
@@ -77,6 +92,7 @@ const Home = () => {
 	const rotating = useRef<HTMLElement | null>(null);
 	const [ activeIndex, setActiveIndex ] = useState(0);
 	const [ intervalId, setIntervalId ] = useState(0);
+	const gap = .1 * parseFloat( getComputedStyle( window.document.body ).fontSize );
 
 	const handleClick = () => {
 		if ( intervalId > 0 ) {
@@ -84,8 +100,9 @@ const Home = () => {
 			setIntervalId(0);
 		} else {
 			{/* @ts-expect-error: It is insisting on the type being NodeJS.Timeout */}
-			const interval: number = setInterval(changeTitle, 2500);
+			const interval: number = setInterval(changeTitle, 2500) as number;
 			setIntervalId(interval);
+			scrollToActive();
 		}
 	}
 
@@ -99,9 +116,9 @@ const Home = () => {
 		setActiveIndex( randomIndex );
 	}
 
-	useLayoutEffect(() => {
+	useEffect(() => {
 		{/* @ts-expect-error: It is insisting on the type being NodeJS.Timeout */}
-		const interval: number = setInterval(changeTitle, 2500);
+		const interval: number = setInterval(changeTitle, 2500) as number;
 		setIntervalId(interval);
 	
 		return () => {
@@ -110,14 +127,29 @@ const Home = () => {
 	}, [])
 
 	const setWidthOfParent = () => {
-		if ( !!rotating && !!rotating.current ) {
-			// Using JS to set the width allows for css transition. Need to get the scrollwidth, as 'width: auto' cannot be transitioned.
-			rotating.current.style.width = `${rotating.current.querySelector('.active')?.scrollWidth}px`;
+		if ( !rotating || !rotating.current ) {
+			return;
 		}
+
+		// Using JS to set the width allows for css transition. Need to get the scrollwidth, as 'width: auto' cannot be transitioned.
+		rotating.current.style.width = `${rotating.current.querySelector('.active')?.scrollWidth}px`;
 	}
 
-	useEffect(() => {
+	const scrollToActive = () => {
+		if ( !rotating || !rotating.current ) {
+			return;
+		}
+
+		const active = rotating.current.querySelector('.active') as HTMLElement;
+		const height = active?.getBoundingClientRect().height;
+		rotating.current.scrollTop = ( height * activeIndex ) + gap;
+	}
+
+	// The blocking of browser paint in layout effects prevents some CLS from width being applied to rotating title.
+	// To make the css transition work, the width can't be set explicitly.
+	useLayoutEffect(() => {
 		setWidthOfParent();
+		scrollToActive();
 	}, [activeIndex])
 
 	return (
@@ -126,14 +158,14 @@ const Home = () => {
 			<big>I build things on the web.</big>
 			<Marquee>
 				I'm a
-				<Rotating className="emphasis box primary" ref={rotating}>
-						{nouns.map((noun, index) => {
-							return (
-								<RotatingTitle className={classNames("rotating-noun", {active: index === activeIndex})} key={noun}>
-									{`${noun}.`}
-								</RotatingTitle>
-							)
-						})}
+				<Rotating className={classNames("emphasis box primary", {paused: intervalId === 0})} ref={rotating}>
+					{nouns.map((noun, index) => {
+						return (
+							<RotatingTitle className={classNames("rotating-noun", {active: index === activeIndex})} key={noun}>
+								{`${noun}.`}
+							</RotatingTitle>
+						)
+					})}
 				</Rotating>
 				<PauseButton onClick={handleClick} data-action={ intervalId > 0 ? 'stop' : 'start'}>
 					<p className="screen-reader-text">
