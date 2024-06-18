c87192cf20f205fe27c2af600841cff60118203a
dl-little <little.douglas22@gmail.com>
2024-06-17 22:47:08 -0400

    Details page

--- resources/js/Pages/Projects/Show.tsx
+++ resources/js/Pages/Projects/Show.tsx
@@ -2,21 +2,38 @@ import { ISharedProps } from '@/Components/interfaces';
 import { IProject } from './Index';
 import styled from "styled-components";
 import Heading from '@/Components/Heading';
-import { Link } from '@inertiajs/react';
+import RenderIf from '@/Components/RenderIf';
+import GHLogo from '@/Components/logos/GHLogo';
 
 import variables from '../../../scss/abstracts/_shared.module.scss';
-const { gap, halfGap, contrast } = variables;
+const { gap, secondary, contrast, tabletBreak, desktopBreak } = variables;
 
 const DetailsContainer = styled.section`
+	display: flex;
+	flex-flow: column nowrap;
+	align-items: flex-end;
+
 	& > *:not(:first-child) {
 		margin-block-start: ${gap};
 	}
 `
 
-const BreadCrumbs = styled.section`
-	& > a, span {
-		font-size: .7em;
-	}
+const ProjectImage = styled.img`
+	display: block;
+	height: 75px;
+	width: 75px;
+	object-fit: cover;
+	border: 2px solid ${secondary};
+
+	@media( min-width: ${tabletBreak} ) {
+        width: 150px;
+		height: 150px;
+    }
+
+	@media( min-width: ${desktopBreak} ) {
+        width: 300px;
+		height: 300px;
+    }
 `;
 
 interface IProjectsDetails extends ISharedProps {
@@ -28,8 +45,20 @@ const Show: React.FC<IProjectsDetails> = ({ project }) => {
 	
     return (
 		<DetailsContainer>
-			<Heading>{`  ${project.title}`}</Heading>
-			<p>{description}</p>
+			<Heading>{title}</Heading>
+			<RenderIf isTrue={!!github_url}>
+				<a href={github_url} className="icon-link" title={`Github link for ${title}`}>
+					<GHLogo
+						primary={contrast}
+						hover={secondary} 
+						width={20}
+					/>
+				</a>
+			</RenderIf>
+			<ProjectImage src={`/storage/${image}`} />
+			<RenderIf isTrue={!!description}>
+				<p>{description}</p>
+			</RenderIf>
 		</DetailsContainer>
     );
 }
