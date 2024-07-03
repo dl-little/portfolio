9cbdcbde001dbd2078726c6c141144ff4a21e56a
dl-little <little.douglas22@gmail.com>
2024-07-03 15:44:22 -0400

    Pass active keys to key

--- resources/js/Components/projects/Synth/components/SynthKeyboard.tsx
+++ resources/js/Components/projects/Synth/components/SynthKeyboard.tsx
@@ -1,5 +1,4 @@
 import Key from './Key';
-import frequencies from '../frequencies.json';
 import triggerData from '../triggers.json';
 import styled from "styled-components";
 import { SynthContext } from './SynthContextProvider';
@@ -16,39 +15,36 @@ const Keyboard = styled.section`
 	position: relative;
 	min-height: 160px;
 
-	& > button:nth-of-type(2) {
-		left:calc(11.11% - 3%);
-	}
-
-	& > button:nth-of-type(4) {
-		left:calc(22.22% - 3%);
-	}
-
-	& > button:nth-of-type(7) {
-		left:calc(44.44% - 3%);
-	}
-
-	& > button:nth-of-type(9) {
-		left:calc(55.55% - 3%);
-	}
-
-	& > button:nth-of-type(11) {
-		left:calc(66.66% - 3%);
-	}
-
-	& > button:nth-of-type(14) {
-		left:calc(88.88% - 3%);
+	& > button.black {
+		&:nth-of-type(2) {
+			left:calc(11.11% - 3%);
+		}
+		&:nth-of-type(4) {
+			left:calc(22.22% - 3%);
+		}
+		&:nth-of-type(7) {
+			left:calc(44.44% - 3%);
+		}
+		&:nth-of-type(9) {
+			left:calc(55.55% - 3%);
+		}
+		&:nth-of-type(11) {
+			left:calc(66.66% - 3%);
+		}
+		&:nth-of-type(14) {
+			left:calc(88.88% - 3%);
+		}
 	}
 `;
 
 export const SynthKeyboard: React.FC = () => {
 	/* @ts-expect-error: TODO: Provide default in definition of context */
-	const { octave, keyboardLetters } = useContext<{octave: "low" | "middle" | "high"}>(SynthContext);
-	const notes = frequencies[octave];
-	const triggers = triggerData.triggers;
+	const { keyboardLetters, activeKeys } = useContext<{octave: "low" | "middle" | "high"}>(SynthContext);
+	const triggers = Object.keys(triggerData);
+
 	return (
 		<Keyboard>
-			{notes.map((note, i) => {
+			{triggers.map((key: string, i: number) => {
 				let color: string;
 				switch (i) {
 					case 1:
@@ -64,12 +60,11 @@ export const SynthKeyboard: React.FC = () => {
 				}
 				return (
 					<Key
-						key={note}
-						frequency={note}
+						key={key}
 						color={color}
-						{...(keyboardLetters && {
-							trigger: triggers[i]
-						})}
+						keyboardLetters={keyboardLetters}
+						trigger={key}
+						active={activeKeys.includes(key)}
 					/>
 				)
 			})}
