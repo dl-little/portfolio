5ea6680d93530f5d504a139bd95ba01dec0ed7e1
dl-little <little.douglas22@gmail.com>
2024-07-08 10:26:35 -0400

    Solve disparity in scrollheight on diff browsers

--- resources/js/Hooks/useScrollOffset.tsx
+++ resources/js/Hooks/useScrollOffset.tsx
@@ -3,18 +3,34 @@ import { useLayoutEffect, useCallback, MutableRefObject } from "react";
 const useScrollOffset = (ref: MutableRefObject<HTMLElement | null>) => {
 
 	const getScrollOffset = useCallback(() => {
-		const articleScrollHeight = ref?.current?.scrollHeight;
-		const articleOffsetHeight = ref?.current?.offsetHeight;
+		if (
+			! ref
+			|| ! ref.current
+			|| ! ref.current.offsetParent
+			|| ! ref.current.firstElementChild
+		) {
+			return;
+		}
+
+		/*
+		 * Calculations for scrollHeight differ dramatically between safari and chrome when display is set to flex.
+		 * Set display to block so that we get a standardized result. Then set it back to flex.
+		 */
+		ref.current.style.display = 'block';
+		const articleScrollHeight = ref.current.scrollHeight;
+		const articleOffsetHeight = ref.current.offsetHeight;
+		ref.current.style.display = 'flex';
+		const gap = .9 * parseFloat( getComputedStyle( window.document.body ).fontSize );
 
 		if (
-			( !articleScrollHeight || !articleOffsetHeight ) ||
 			articleScrollHeight <= articleOffsetHeight
+			// Below accounts for when the article is not taller than the viewport, but the frame overlaps the content.
+			&& ref.current.offsetParent.clientHeight - ref.current.firstElementChild.clientHeight > (gap * 4)
 		) {
 			return 0;
 		}
 
-		const gap = .9 * parseFloat( getComputedStyle( window.document.body ).fontSize );
-		return Math.round((articleScrollHeight + ( gap * 2 )) - articleOffsetHeight );
+		return Math.round(((articleScrollHeight + ( gap * 3 )) - articleOffsetHeight ) / 2 );
 	}, [ref])
 
 	const setBodyAttribute = useCallback(() => {
