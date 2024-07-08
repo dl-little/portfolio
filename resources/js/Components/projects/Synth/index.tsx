0a6bdbbced01e2e3b5494f06d34e820ed0daa4aa
dl-little <little.douglas22@gmail.com>
2024-07-08 08:35:32 -0400

    Only boxshadow on desktop

--- resources/js/Components/projects/Synth/index.tsx
+++ resources/js/Components/projects/Synth/index.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import variables from '../../../../scss/abstracts/_shared.module.scss';
+const { tabletBreak } = variables;
 
 export const SynthWrap = styled.section`
 	width: 100%;
@@ -7,8 +9,10 @@ export const SynthWrap = styled.section`
 	justify-content: center;
 	align-items: center;
 	background-color: #B1B1B1;
-	-webkit-box-shadow: 5px 5px 5px #222222;
-	box-shadow: 5px 5px 5px #222222;
+	@media (min-width: ${tabletBreak}) {
+		-webkit-box-shadow: 5px 5px 5px #222222;
+		box-shadow: 5px 5px 5px #222222;
+	}
 `;
 
 export {SynthControls} from './components/SynthControls';
